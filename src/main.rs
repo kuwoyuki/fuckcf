2381dab284312f04e3b7dc5bb7bc44a84e0cf686
kuwoyuki <kuwoyuki@gmail.com>
2023-03-08 01:00:38 +0600

    chore: _

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,13 @@ async fn configure_headless(driver: &WebDriver) {
     println!("adding chrome evasions");
     dev_tools
     .execute_cdp_with_params(
-                    // navigator props
-            // Object.defineProperty(navigator, "maxTouchPoints", { get: () => 1 });
-            // Object.defineProperty(navigator.connection, "rtt", { get: () => 100 });
-
         "Page.addScriptToEvaluateOnNewDocument",
         json!({
             "source": r#"
+            // navigator props
+            Object.defineProperty(navigator, "maxTouchPoints", { get: () => 1 });
+            Object.defineProperty(navigator.connection, "rtt", { get: () => 100 });
+
             // https://github.com/microlinkhq/browserless/blob/master/packages/goto/src/evasions/window-frame.js
             if (window.outerWidth && window.outerHeight) return;
             const windowFrame = 85;
@@ -457,59 +457,251 @@ async fn configure_headless(driver: &WebDriver) {
     .unwrap();
 }
 
+async fn solve_incolumitas(driver: &WebDriver) {
+    driver.goto("https://bot.incolumitas.com").await.unwrap();
+    let page = driver.query(By::Id("formStuff")).first().await.unwrap();
+
+    let username_element = page.find(By::Css(r#"[name="userName"]"#)).await.unwrap();
+    username_element.click().await.unwrap();
+    username_element.clear().await.unwrap();
+    username_element.send_keys("bot3000").await.unwrap();
+
+    // same stuff here
+    let email_element = page.find(By::Css(r#"[name="eMail"]"#)).await.unwrap();
+    email_element.click().await.unwrap();
+    email_element.clear().await.unwrap();
+    email_element.send_keys("bot3000@gmail.com").await.unwrap();
+
+    // cookie element
+    let cookie_element = page.find(By::Css(r#"[name="cookies"]"#)).await.unwrap();
+    cookie_element.click().await.unwrap();
+    cookie_element
+        .send_keys("I want all the Cookies")
+        .await
+        .unwrap();
+
+    // smol cat
+    let el = driver.find(By::Id("smolCat")).await.unwrap();
+    el.click().await.unwrap();
+    // big cat
+    let el = driver.find(By::Id("bigCat")).await.unwrap();
+    el.click().await.unwrap();
+    // submit
+    let el = driver.find(By::Id("submit")).await.unwrap();
+    el.click().await.unwrap();
+
+    driver.accept_alert().await.unwrap();
+
+    // wait for results to appear
+    driver
+        .query(By::Css("#tableStuff tbody tr .url"))
+        .first()
+        .await
+        .unwrap();
+    // just in case
+    sleep(Duration::from_millis(100)).await;
+
+    let p = page
+        .query(By::Css("#tableStuff tbody tr .url"))
+        .first()
+        .await
+        .unwrap();
+
+    // now update both prices
+    // by clicking on the "Update Price" button
+    //  await page.waitForSelector('#updatePrice0');
+    //  await page.click('#updatePrice0');
+    //  await page.waitForFunction('!!document.getElementById("price0").getAttribute("data-last-update")');
+
+    //  await page.waitForSelector('#updatePrice1');
+    //  await page.click('#updatePrice1');
+    //  await page.waitForFunction('!!document.getElementById("price1").getAttribute("data-last-update")');
+
+    // Type in the search terms.
+    //  elem_text.send_keys("selenium").await?;
+
+    //  // Click the search button.
+    //  let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+    //  elem_button.click().await?;
+
+    //  // Look for header to implicitly wait for the page to load.
+    //  driver.find(By::ClassName("firstHeading")).await?;
+    //  assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+
+    // overwrite the existing text by selecting it
+    // with the mouse with a triple click
+
+    // const userNameInput = await page.$('[name="userName"]');
+}
+
+async fn configure_headful(driver: &WebDriver) {
+    let dev_tools = ChromeDevTools::new(driver.handle.clone());
+
+    println!("adding chrome evasions");
+    dev_tools
+  .execute_cdp_with_params(
+      "Page.addScriptToEvaluateOnNewDocument",
+      json!({
+          "source": r#"
+          // monkey patch Object.defineProperty
+          const { defineProperty } = window.Object;
+          window.Object.defineProperty = function (o, p, attrs) {
+            if (o instanceof Error && o.stack && p === "stack") {
+              return o;
+            }
+            return defineProperty.call(this, o, p, attrs);
+          };
+          // https://github.com/microlinkhq/browserless/blob/master/packages/goto/src/evasions/webgl-vendor.js
+          // Remove traces of our Proxy ;-)
+          const stripErrorStack = (stack) =>
+            stack
+              .split("\n")
+              .filter((line) => !line.includes("at Object.apply"))
+              .filter((line) => !line.includes("at Object.get"))
+              .join("\n");
+          
+          const getParameterProxyHandler = {
+            get(target, key) {
+              try {
+                // Mitigate Chromium bug (#130)
+                if (typeof target[key] === "function") {
+                  return target[key].bind(target);
+                }
+                return Reflect.get(target, key);
+              } catch (err) {
+                err.stack = stripErrorStack(err.stack);
+                throw err;
+              }
+            },
+            apply: function (target, thisArg, args) {
+              const param = (args || [])[0];
+              // UNMASKED_VENDOR_WEBGL
+              if (param === 37445) return "Intel Inc.";
+              // UNMASKED_RENDERER_WEBGL
+              if (param === 37446) return "Intel(R) Iris(TM) Plus Graphics 640";
+              try {
+                return Reflect.apply(target, thisArg, args);
+              } catch (err) {
+                err.stack = stripErrorStack(err.stack);
+                throw err;
+              }
+            },
+          };
+          
+          ["WebGLRenderingContext", "WebGL2RenderingContext"].forEach(function (ctx) {
+            Object.defineProperty(window[ctx].prototype, "getParameter", {
+              configurable: true,
+              enumerable: false,
+              writable: false,
+              value: new Proxy(
+                window[ctx].prototype.getParameter,
+                getParameterProxyHandler
+              ),
+            });
+          });          
+          "#
+      }),
+  )
+  .await
+  .unwrap();
+}
+
+// https://hmaker.github.io/selenium-detector/
+async fn solve_selenium_detector(driver: &WebDriver) {
+    driver
+        .goto("https://hmaker.github.io/selenium-detector/")
+        .await
+        .unwrap();
+
+    let token_v = driver
+        .execute("return window.token;", Vec::new())
+        .await
+        .unwrap()
+        .convert::<String>()
+        .unwrap();
+
+    let token = driver.find(By::Id("chromedriver-token")).await.unwrap();
+    token.send_keys(token_v).await.unwrap();
+
+    let token_v = driver
+        .execute("return window.getAsyncToken();", Vec::new())
+        .await
+        .unwrap()
+        .convert::<String>()
+        .unwrap();
+    println!("{:?}", token_v);
+
+    let async_token = driver
+        .find(By::Id("chromedriver-asynctoken"))
+        .await
+        .unwrap();
+    async_token.send_keys(token_v).await.unwrap();
+
+    let submit = driver.find(By::Id("chromedriver-test")).await.unwrap();
+    submit.click().await.unwrap();
+}
+
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
     let mut caps = DesiredCapabilities::chrome();
     //  caps.add_chrome_option("remote-debugging-port", "9222");
     // caps.add_chrome_option("debuggerAddress", "127.0.0.1:9222")?;
-    caps.add_chrome_arg("--autoplay-policy=user-gesture-required")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
-    caps.add_chrome_arg("--disable-blink-features=AutomationControlled")?; // https://blog.m157q.tw/posts/2020/09/11/bypass-cloudflare-detection-while-using-selenium-with-chromedriver/
-    caps.add_chrome_arg("--disable-cloud-import")?;
-    caps.add_chrome_arg("--disable-component-update")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
-    caps.add_chrome_arg("--disable-domain-reliability")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
-    caps.add_chrome_arg(
-        "--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process",
-    )?; // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
-    caps.add_chrome_arg("--disable-blink-features=AutomationControlled")?;
-    caps.add_chrome_arg("--disable-gesture-typing")?;
-    caps.add_chrome_arg("--disable-infobars")?;
-    caps.add_chrome_arg("--disable-notifications")?;
-    caps.add_chrome_arg("--disable-offer-store-unmasked-wallet-cards")?;
-    caps.add_chrome_arg("--disable-offer-upload-credit-cards")?;
-    caps.add_chrome_arg("--disable-print-preview")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
-    caps.add_chrome_arg("--disable-setuid-sandbox")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
-    caps.add_chrome_arg("--disable-site-isolation-trials")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
-    caps.add_chrome_arg("--disable-speech-api")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
-    caps.add_chrome_arg("--disable-tab-for-desktop-share")?;
-    caps.add_chrome_arg("--disable-translate")?;
-    caps.add_chrome_arg("--disable-voice-input")?;
-    caps.add_chrome_arg("--disable-wake-on-wifi")?;
-    caps.add_chrome_arg("--enable-async-dns")?;
-    caps.add_chrome_arg("--enable-simple-cache-backend")?;
-    caps.add_chrome_arg("--enable-tcp-fast-open")?;
-    caps.add_chrome_arg("--enable-webgl")?;
-    caps.add_chrome_arg("--force-webrtc-ip-handling-policy=default_public_interface_only")?;
-    caps.add_chrome_arg("--ignore-gpu-blocklist")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
-    caps.add_chrome_arg("--no-default-browser-check")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
-    caps.add_chrome_arg("--no-pings")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
-    caps.add_chrome_arg("--no-sandbox")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
-    caps.add_chrome_arg("--no-zygote")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
-    caps.add_chrome_arg("--prerender-from-omnibox=disabled")?;
-    caps.add_chrome_arg("--use-gl=swiftshader")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
-                                                  // caps.add_chrome_arg("--remote-debugging-port=9222")?;
-    caps.add_chrome_arg("--window-size=1920,1080")?;
-    caps.add_chrome_arg("--start-maximized")?;
-    caps.add_chrome_arg("--ozone-platform-hint=auto")?;
+    // caps.set_binary("/usr/lib/chromium/chromium")?;
+    // caps.add_arg("--user-data-dir=/home/mira/.temp_webdriver")?;
+    caps.add_arg("--autoplay-policy=user-gesture-required")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
+    caps.add_exclude_switch("enable-automation")?;
+    caps.add_arg("--disable-blink-features=AutomationControlled")?; // https://blog.m157q.tw/posts/2020/09/11/bypass-cloudflare-detection-while-using-selenium-with-chromedriver/
+                                                                    // caps.add_arg("--disable-cloud-import")?;
+                                                                    // caps.add_arg("--disable-component-update")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
+                                                                    // caps.add_arg("--disable-domain-reliability")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
+                                                                    // caps.add_arg(
+                                                                    // "--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process",
+                                                                    // )?; // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+                                                                    // caps.add_arg("--disable-gesture-typing")?;
+                                                                    // caps.add_arg("--disable-infobars")?;
+                                                                    // caps.add_arg("--disable-notifications")?;
+                                                                    // caps.add_arg("--disable-offer-store-unmasked-wallet-cards")?;
+                                                                    // caps.add_arg("--disable-offer-upload-credit-cards")?;
+                                                                    // caps.add_arg("--disable-print-preview")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
+                                                                    // caps.add_arg("--disable-setuid-sandbox")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
+                                                                    //                                            // cloudflare challenge doesn't pass with this disabled
+                                                                    //                                            // caps.add_arg("--disable-site-isolation-trials")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
+                                                                    // caps.add_arg("--disable-speech-api")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
+                                                                    // caps.add_arg("--disable-tab-for-desktop-share")?;
+                                                                    // caps.add_arg("--disable-translate")?;
+                                                                    // caps.add_arg("--disable-voice-input")?;
+                                                                    // caps.add_arg("--disable-wake-on-wifi")?;
+                                                                    // caps.add_arg("--enable-async-dns")?;
+                                                                    // caps.add_arg("--enable-simple-cache-backend")?;
+                                                                    // caps.add_arg("--enable-tcp-fast-open")?;
+                                                                    // caps.add_arg("--enable-webgl")?;
+                                                                    // caps.add_arg("--force-webrtc-ip-handling-policy=default_public_interface_only")?;
+                                                                    // caps.add_arg("--ignore-gpu-blocklist")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
+                                                                    // caps.add_arg("--no-default-browser-check")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
+                                                                    // caps.add_arg("--no-pings")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
+    caps.add_arg("--no-sandbox")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
+                                   // caps.add_arg("--no-zygote")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
+                                   // caps.add_arg("--prerender-from-omnibox=disabled")?;
+    caps.add_arg("--use-gl=desktop")?; // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
+    caps.add_arg("--remote-debugging-port=9222")?;
+    caps.add_arg("--window-size=1920,1080")?;
+    caps.add_arg("--start-maximized")?;
+    // caps.add_arg("--ozone-platform-hint=wayland")?;
     // caps.set_headless()?;
     caps.set_no_sandbox()?;
-    // caps.set_disable_gpu()?;
-    // caps.set_disable_dev_shm_usage()?;
+    caps.set_disable_gpu()?;
+    caps.set_disable_dev_shm_usage()?;
+    // caps.add_arg("--headless=new")?;
     // caps.set_debugger_address("127.0.0.1:9222")?;
     let driver = WebDriver::new("http://localhost:4444", caps).await?;
 
     // configure_headless(&driver).await;
+    configure_headful(&driver).await;
+    // solve_incolumitas(&driver).await;
+    solve_selenium_detector(&driver).await;
 
-    driver.goto("https://abrahamjuliot.github.io/creepjs").await?;
+    // driver.goto("https://detect.azerpas.com/").await?;
+    // driver.got
     // driver
     //     .screenshot(Path::new("./nowsecure1.png"))
     //     .await
@@ -525,14 +717,16 @@ async fn main() -> WebDriverResult<()> {
     //     .await
     //     .unwrap();
     // sleep(Duration::from_secs(8)).await;
-    // driver
-    //     .screenshot(Path::new("./nowsecure4.png"))
-    //     .await
-    // .unwrap();
-    sleep(Duration::from_secs(360)).await;
+    // driver.screenshot(Path::new("./creepjs.png")).await.unwrap();
+
+    // Find element from element.
+    // let elem_text = driver.find(By::Css(".px-3 > h1:nth-child(1)")).await?.text().await?;
+    // println!("{}", elem_text);
+
+    // sleep(Duration::from_secs(360)).await;
 
     // Always explicitly close the browser.
-    driver.quit().await?;
+    // driver.quit().await?;
 
     Ok(())
 }
